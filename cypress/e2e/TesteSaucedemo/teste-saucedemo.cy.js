9053fed30fa079f3d0b1f663f96407e3b3d08652
Alexandru Prodan <alexprodan711@gmail.com>
2023-04-07 22:49:47 +0300

    Added Saucedemo tests

---
+++ cypress/e2e/TesteSaucedemo/teste-saucedemo.cy.js
@@ -0,0 +1,98 @@
+describe ('Teste Saucedemo', () => {
+
+    // Valid credentials
+    it('Login test with valid credentials', () => {
+
+        // Fix found online for the known 'load' event error on Saucedemo
+        cy.intercept('/service-worker.js', {
+            body: undefined
+           })
+
+
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user'); 
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+
+        cy.url().should('include', '/inventory.html')
+    })
+
+
+    // Invalid credentials
+    it('Login test with invalid credentials', () => {
+
+        // Fix found online for the known 'load' event error on Saucedemo
+        cy.intercept('/service-worker.js', {
+            body: undefined
+           })
+
+
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('invalid'); 
+        cy.get('[data-test="password"]').type('invalid');
+        cy.get('[data-test="login-button"]').click();
+
+        cy.get('[data-test="error"]').should('be.visible');
+    })
+
+
+    // Logout
+    it('Logout test', () => {
+
+        // Fix found online for the known 'load' event error on Saucedemo
+        cy.intercept('/service-worker.js', {
+            body: undefined
+           })
+
+
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user'); 
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#logout_sidebar_link').click();
+
+        cy.get('[data-test="login-button"]').should('exist');
+    })
+
+
+   // Sidebar menu functionality
+   it('Sidebar menu functionality test', () => {
+
+    // Fix found online for the known 'load' event error on Saucedemo
+    cy.intercept('/service-worker.js', {
+        body: undefined
+       })
+
+
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').type('standard_user'); 
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+    cy.get('#react-burger-menu-btn').click();
+    cy.get('#react-burger-cross-btn').click();
+
+    cy.get('.bm-menu-wrap').should('be.hidden');
+    })
+
+   // Add product to cart
+   it('Add a product to cart', () => {
+
+    // Fix found online for the known 'load' event error on Saucedemo
+    cy.intercept('/service-worker.js', {
+        body: undefined
+       })
+
+
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test="username"]').type('standard_user'); 
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    cy.get('.shopping_cart_badge').should('be.visible');
+    })
+
+
+
+})
