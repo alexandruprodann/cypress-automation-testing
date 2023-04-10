0ad99920c2242bf99ecea442ca5de1dc23e2729f
Alexandru Prodan <100951219+alexprodann@users.noreply.github.com>
2023-04-10 17:31:43 +0300

    Add viewport

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl, etc
     viewportWidth: 1200,
     viewportHeight: 1200,
     setupNodeEvents(on, config) {
